7f2a0543d9897fbb9facc94a38f37c6f9ae51879
Felipe <fperezferraro@gmail.com>
2023-09-20 15:14:32 -0300

    Updated queries.sql

--- queries.sql
+++ queries.sql
@@ -100,29 +100,6 @@ ADD COLUMN species_id INTEGER REFERENCES species(id);
 ALTER TABLE animals
 ADD COLUMN owners_id INTEGER REFERENCES owners(id);
 
--- If the name ends in "mon" it will be Digimon
--- All other animals are Pokemon
-UPDATE animals
-SET species_id = CASE
-	WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
-	ELSE (SELECT id FROM species WHERE name = 'Pokemon')
-END;
-
--- Modify your inserted animals to include owner information (owner_id):
--- Sam Smith owns Agumon.
--- Jennifer Orwell owns Gabumon and Pikachu.
--- Bob owns Devimon and Plantmon.
--- Melody Pond owns Charmander, Squirtle, and Blossom.
--- Dean Winchester owns Angemon and Boarmon.
-UPDATE animals
-SET owners_id = CASE
-	WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
-	WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
-	WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
-	WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
-	WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
-END;
-
 -- Write queries (using JOIN) to answer the following questions:
 -- What animals belong to Melody Pond?
 SELECT a.name AS animal_name
