81a021202cf62af4beae2418155e72dd4bf150a4
Felipe <fperezferraro@gmail.com>
2023-09-20 15:14:22 -0300

    Updated data.sql

--- data.sql
+++ data.sql
@@ -24,7 +24,32 @@ VALUES
 	('Dean Winchester', 14),
 	('Jodie Whittaker', 38);
 
+-- Modify your inserted animals to include owner information (owner_id):
+-- Sam Smith owns Agumon.
+-- Jennifer Orwell owns Gabumon and Pikachu.
+-- Bob owns Devimon and Plantmon.
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+-- Dean Winchester owns Angemon and Boarmon.
+UPDATE animals
+SET owners_id = CASE
+	WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+	WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+	WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+	WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+	WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+END
+WHERE owners_id IS NULL;
+
 INSERT INTO species(name)
 VALUES
 	('Pokemon'),
 	('Digimon');
+
+-- If the name ends in "mon" it will be Digimon
+-- All other animals are Pokemon
+UPDATE animals
+SET species_id = CASE
+	WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+	ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+END
+WHERE species_id IS NULL;
