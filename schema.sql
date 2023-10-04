f8740a25c8f4ebad87e81b4d5884e3d55510619c
Felipe <fperezferraro@gmail.com>
2023-10-04 18:05:58 -0300

    Created indexes to improve the speed

--- schema.sql
+++ schema.sql
@@ -45,3 +45,8 @@ CREATE TABLE visits (
 
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- Create indexes to improve the speed
+CREATE INDEX idx_animal_id ON visits(animal_id);
+CREATE INDEX idx_vet_id ON visits(vet_id);
+CREATE INDEX idx_email ON owners(email);
